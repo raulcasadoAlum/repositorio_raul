eeaf27abde84b25e37b1b385eea03eaf7803bb6e
raulcasadoAlum <rcasmor1010@g.educaand.es>
2022-05-21 18:34:50 +0200

    gitignore

---
+++ ejemplosInicialesJAVA-20220519T101726Z-001/ejemplosInicialesJAVA/WhileCopia.java
@@ -0,0 +1,19 @@
+package ejemplosInicialesJAVA;
+
+public class While {
+
+	public static void main(String[] args) {
+		int num=1;
+		System.out.println("Contando...");
+		
+		
+		while (num<=10) {
+			System.out.println("Vamos por el n�mero " + num);
+			num++;
+		}
+
+		System.out.println("Fin de la cuenta");
+		System.out.println("Hola, me llamo Raúl Casado Moreno");
+	}
+
+}
